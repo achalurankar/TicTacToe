db502519fe36dc7ea9cabde3016a7dcfc3077c31
Aochocinco <achalurankar@gmail.com>
2020-09-27 16:57:30 +0530

    Second Commit

--- app/src/main/java/com/android/tictactoe/Game.java
+++ app/src/main/java/com/android/tictactoe/Game.java
@@ -1,13 +1,12 @@
 package com.android.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +18,7 @@ public class Game extends AppCompatActivity {
     char[][] mat = new char[3][3];
     List<Character> turnsList = new ArrayList<>();
     int turnCount = 0;
-    TextView result,restart;
+    TextView result, restart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,155 +55,79 @@ public class Game extends AppCompatActivity {
         x0y0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[0][0] == ' ') {
-                    mat[0][0] = turn;
-                    if (turn == 'x') {
-                        x0y0.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x0y0.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(0, 0, x0y0);
             }
         });
 
         x0y1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[0][1] == ' ') {
-                    mat[0][1] = turn;
-                    if (turn == 'x') {
-                        x0y1.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x0y1.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(0, 1, x0y1);
             }
         });
 
         x0y2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[0][2] == ' ') {
-                    mat[0][2] = turn;
-                    if (turn == 'x') {
-                        x0y2.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x0y2.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(0, 2, x0y2);
             }
         });
 
         x1y0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[1][0] == ' ') {
-                    mat[1][0] = turn;
-                    if (turn == 'x') {
-                        x1y0.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x1y0.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(1, 0, x1y0);
             }
         });
 
         x1y1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[1][1] == ' ') {
-                    mat[1][1] = turn;
-                    if (turn == 'x') {
-                        x1y1.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x1y1.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(1, 1, x1y1);
             }
         });
 
         x1y2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[1][2] == ' ') {
-                    mat[1][2] = turn;
-                    if (turn == 'x') {
-                        x1y2.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x1y2.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(1, 2, x1y2);
             }
         });
 
         x2y0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[2][0] == ' ') {
-                    mat[2][0] = turn;
-                    if (turn == 'x') {
-                        x2y0.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x2y0.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(2, 0, x2y0);
             }
         });
 
         x2y1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[2][1] == ' ') {
-                    mat[2][1] = turn;
-                    if (turn == 'x') {
-                        x2y1.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x2y1.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(2, 1, x2y1);
             }
         });
 
         x2y2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mat[2][2] == ' ') {
-                    mat[2][2] = turn;
-                    if (turn == 'x') {
-                        x2y2.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
-                        turn = 'o';
-                    } else {
-                        x2y2.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
-                        turn = 'x';
-                    }
-                    checkWinner();
-                }
+                Game.this.onClick(2, 2, x2y2);
             }
         });
+    }
+
+    private void onClick(int i, int j, ImageView block) {
+        if (mat[i][j] == ' ') {
+            mat[i][j] = turn;
+            if (turn == 'x') {
+                block.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.x_vector));
+                turn = 'o';
+            } else {
+                block.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.o_vector));
+                turn = 'x';
+            }
+            checkWinner();
+        }
     }
 
     private void initializeSpacesToMat() {
@@ -217,44 +140,42 @@ public class Game extends AppCompatActivity {
 
     private void checkWinner() {
         turnCount++;
+        char a, b, c;
         for (int i = 0; i < 3; i++) {
-            char a, b, c;
             a = mat[i][0];
             b = mat[i][1];
             c = mat[i][2];
 
-            boolean res = isWinner(a,b,c);
-            if(res)
+            boolean res = isWinner(a, b, c);
+            if (res)
                 return;
 
             a = mat[0][i];
             b = mat[1][i];
             c = mat[2][i];
 
-            res = isWinner(a,b,c);
-            if(res)
+            res = isWinner(a, b, c);
+            if (res)
                 return;
         }
-
-        char a, b, c;
 
         a = mat[0][0];
         b = mat[1][1];
         c = mat[2][2];
 
-        boolean res = isWinner(a,b,c);
-        if(res)
+        boolean res = isWinner(a, b, c);
+        if (res)
             return;
 
         a = mat[2][0];
         b = mat[1][1];
         c = mat[0][2];
 
-        res = isWinner(a,b,c);
-        if(res)
+        res = isWinner(a, b, c);
+        if (res)
             return;
 
-        if(turnCount == 9){
+        if (turnCount == 9) {
             restart.setVisibility(View.VISIBLE);
             result.setText("Draw");
         }
